4fe3ee5699908b45f6aa67e060e3b5fc35e4eacc
tonyfleck76 <tonyfleck76@gmail.com>
2015-05-02 16:17:32 -0500

    Third commit, changed config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ module.exports = {
 
     authorizeUrl: 'https://secure.api.comcast.net/X1/oauth2/authorize',
     /* Put oauth redirectUri, which is from portal application*/
-    redirectUri: 'XXX',
+    redirectUri: 'https://intxtonyadam.azurewebsites.net/oauthSuccess',
     /* Put clientId, which is from portal application*/
-    clientId: 'XXX',
+    clientId: '8n8u9sk6k8dhdt4mpg6jbc8c',
     /* Put clientSecret, which is from portal application*/
-    clientSecret: 'XXX',
+    clientSecret: 'CgAN3j3Z',
     scope: 'devices.lookup content.search devices.tune devices.send_notification',
 
     getAccessToken: {
